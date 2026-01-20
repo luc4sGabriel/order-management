90dbc7f161d8ae92166e59bb280b294fc12463ca
luc4sGabriel <lucas.g4briel.04@gmail.com>
2026-01-20 15:01:20 -0300

    build: add cors config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import express from "express";
 import routes from "./routes/index.routes";
 import { errorHandler } from "./middlewares/error-handler";
+import cors from "cors";
 
 export const app = express();
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://order-management-il5c.onrender.com',
+    // FlutterFlow link
+  ],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());
 app.use(routes);
